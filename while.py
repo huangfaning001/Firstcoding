39640035463a9cd3da493b82a5a42f9ae5a264e6
FaningHuang <33030518+HuangFaning@users.noreply.github.com>
2017-10-26 22:43:49 +0800

    使用while来列出奇偶数 、猜年龄

---
+++ while.py
@@ -0,0 +1,59 @@
+
+#num = 1
+#while num <= 10:
+#    print(num)
+#    num += 1 
+	
+	
+#num = 0
+#while num <= 100:
+#    print(num)
+#    num += 2
+
+#num = 1
+#while num <= 100:
+#    if num%2 == 0:
+#        print(num)
+#    num += 1
+
+#num = 1
+#while num <= 100:
+#    print(num)
+#    num += 2
+
+#num = 1
+#while num <= 100:
+#    if num%2 == 1:
+#        print(num)
+#    num += 1
+
+#age = 45
+
+#flag = True
+
+#while flag:
+#    user_input_age = int(input("Please input age: "))
+#    if user_input_age == age:
+#        print("Yes,it's right")
+#        flag = False
+#    elif user_input_age > age:
+#        print("Is bigger")
+#    else:
+#        print("Is smaller")
+#print("End")
+
+
+age = 45
+# break 终止
+flag = True
+
+while flag:
+    user_input_age = int(input("Please input age: "))
+    if user_input_age == age:
+        print("Yes,it's right")
+        break
+    elif user_input_age > age:
+        print("Is bigger")
+    else:
+        print("Is smaller")
+print("End")
