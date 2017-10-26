3e8da989a3ddc19fb3b5f354391979b167e25869
FaningHuang <33030518+HuangFaning@users.noreply.github.com>
2017-10-26 21:32:38 +0800

    renew

--- Max_Min.py
+++ Max_Min.py
@@ -30,3 +30,25 @@ else:
 #        print("Max NUM is",max_num)
 #    else:
 #        print("Max NUM is",num3)
+
+
+
+
+
+#num1 = input("Please input num1: ")
+#num2 = input("Please input num2: ")
+#num3 = input("Please input num3: ")
+
+#min_num = 0
+
+#if num1 < num2:
+#    min_num = num1
+#    if min_num > num3:
+#        print("The min_num is ",num3)
+#    else:
+#        print("The min_num is ",min_num)
+#else:
+#    if num2 < num3:
+#        print("The min_num is ",min_num)
+#    else:
+#        print("The min_num is ",num3)
