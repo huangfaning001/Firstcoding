15c01788850fac172da46d435e7dcbad0b08a12f
FaningHuang <33030518+HuangFaning@users.noreply.github.com>
2017-10-30 22:38:36 +0800

    Pycharm模板日期

---
+++ Pycharm template date.py
@@ -0,0 +1,52 @@
+#__author__:'Faning Huang'
+#__time__:'2017/10/30 0030 下午 10:25'
+
+
+#Pycharm新建模板默认添加作者时间等信息
+
+#pycharm新建文件时的头部模板，即默认的：author = ‘$USER’，pycharm下依次“File→settings→Editor→File and Code Template”即可找到界面
+
+#预定义的变量要扩展为格式为$ {<variable_name>}的相应值。
+
+#可用的预定义文件模板变量为：
+
+#$ {PROJECT_NAME} - 当前项目的名称。
+
+#$ {NAME} - 在文件创建过程中在“新建文件”对话框中指定的新文件的名称。
+
+#$ {USER} - 当前用户的登录名。
+
+#$ {DATE} - 当前的系统日期。
+
+#$ {TIME} - 当前系统时间。
+
+#$ {YEAR} - 今年。
+
+#$ {MONTH} - 当月。
+
+#$ {DAY} - 当月的当天。
+
+#$ {HOUR} - 目前的小时。
+
+#$ {MINUTE} - 当前分钟。
+
+#$ {PRODUCT_NAME} - 将在其中创建文件的IDE的名称。
+
+#$ {MONTH_NAME_SHORT} - 月份名称的前3个字母。 示例：1月，2月等
+
+#$ {MONTH_NAME_FULL} - 一个月的全名。 示例：1月，2月等
+
+#English
+#${PROJECT_NAME} - the name of the current project.
+#${NAME} - the name of the new file which you specify in the New File dialog box during the file creation.
+#${USER} - the login name of the current user.
+#${DATE} - the current system date.
+#${TIME} - the current system time.
+#${YEAR} - the current year.
+#${MONTH} - the current month.
+#${DAY} - the current day of the month.
+#${HOUR} - the current hour.
+#${MINUTE} - the current minute.
+#${PRODUCT_NAME} - the name of the IDE in which the file will be created.
+#${MONTH_NAME_SHORT} - the first 3 letters of the month name. Example: Jan, Feb, etc.
+#${MONTH_NAME_FULL} - full name of a month. Example: January, February, etc.
