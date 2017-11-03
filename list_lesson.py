ef07ecfb08d01b9e33f54eb6167584329b3e15ed
FaningHuang <33030518+HuangFaning@users.noreply.github.com>
2017-11-03 22:38:43 +0800

    增删改查

---
+++ list_lesson.py
@@ -0,0 +1,57 @@
+#__author__:'Faning Huang'
+#__time__:'2017/11/3 0003 下午 9:48'
+
+name = 'wc'
+name1 = 'jx'
+name2 = 'xh'
+name3 = 'sp'
+name4 = 'lg'
+
+#names = 'wc jx xh sp lg'
+
+a = ['wc','jx','xh','sp','lg']
+
+# 增删改查
+
+#增 切片
+
+# print (a[1:]) #取到最后
+# print (a[1:-1]) #取到倒数第二值
+# print (a[1:-1:1]) #从左到右一个一个去取
+# print (a[1::2]) #从左到右隔一个去取
+# print (a[-2::-2])
+
+
+# 添加 append insert
+
+# a.append('xp') #默认插到最后一个位置
+#print(a)
+
+# a.insert(1,'hh') #将数据 插入到任意一个位置
+# print(a)
+
+#修改
+
+# a[1]='xm'
+# print(a)
+
+# a[1:3]=['ab','cd']
+# print(a)
+
+#删除 remove pop del
+
+# a.remove('wc')
+# print(a)
+
+# a.pop(2)
+# print(a)
+
+# del a[0]
+# print(a)
+
+del a
+print(a)
+
+
+
+
