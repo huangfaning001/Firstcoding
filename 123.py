3848a2ec7d0408cb8875a554bbc7098f649a4c52
FaningHuang <33030518+HuangFaning@users.noreply.github.com>
2017-11-17 22:27:25 +0800

    renew

--- 123.py
+++ 123.py
@@ -24,4 +24,38 @@ def register():
             else:
                 print("passwd1 != passwd2")
                 register()
+    except UnboundLocalError as e:
+        pass
+
+if _name_=="_main_":
+    login_user = input("name>>")
+    f1=open("black","r+",encoding="utf-8")
+    for line in f.readlines():
+        if login_user in line:
+            user,passwd = line.strip('\n').split(':')
+            for i in range(3):
+                login_password = input("password>>")
+                if login_user == user and login_password == passwd:
+                    print("welcome %s login_user")
+                    f.close()
+                    break
+                else:
+                    f1 = open("black",encoding="utf-8")
+                    f1.writelines('\n')
+                    f1.writelines(login_user)
+                    f1.close()
+                    print("you are in black now")
+                break
+            else:
+                print("没有注册，是否注册？1：注册  其他任意字符退出" )
+                try:
+                    choice = int(input("choose>>"))
+                    if choice !=1:
+                        pass
+                    else:
+                        retister()
+                except ValueError as e:
+                    pass
+
+
 
