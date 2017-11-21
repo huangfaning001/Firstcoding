faf6a5f66a7dd7da2f175fb18c634945b7087251
FaningHuang <33030518+HuangFaning@users.noreply.github.com>
2017-11-21 21:49:45 +0800

    renew

--- Shortcut key.py
+++ Shortcut key.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #__author__:'Faning Huang'
 #__time__:'2017/11/18 0018 下午 10:25'
 
@@ -24,3 +25,31 @@ PyCharm常用快捷键
 # Ctrl + NumPad+/- 展开或者收缩代码块
 # Ctrl + Shift + NumPad+ 展开所有的代码块
 # Ctrl + Shift + NumPad- 收缩所有的代码块
+=======
+#__author__:'Faning Huang'
+#__time__:'2017/11/18 0018 下午 10:25'
+
+PyCharm常用快捷键
+
+
+
+
+# Alt+Enter 自动添加包
+# Ctrl+t SVN更新
+# Ctrl+k SVN提交
+# Ctrl + / 注释(取消注释)选择的行
+# Ctrl+Shift+F 高级查找
+# Ctrl+Enter 补全
+# Shift + Enter 开始新行
+# TAB Shift+TAB 缩进/取消缩进所选择的行
+# Ctrl + Alt + I 自动缩进行
+# Ctrl + Y 删除当前插入符所在的行
+# Ctrl + D 复制当前行、或者选择的块
+# Ctrl + Shift + J 合并行
+# Ctrl + Shift + V 从最近的缓存区里粘贴
+# Ctrl + Delete 删除到字符结尾
+# Ctrl + Backspace 删除到字符的开始
+# Ctrl + NumPad+/- 展开或者收缩代码块
+# Ctrl + Shift + NumPad+ 展开所有的代码块
+# Ctrl + Shift + NumPad- 收缩所有的代码块
+>>>>>>> 39c2212e8f3d36c2375e0176700d851aebefe0cb
