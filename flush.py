5cb12dac0f44d5c775186f488f752e87dd6927ab
FaningHuang <33030518+HuangFaning@users.noreply.github.com>
2017-11-23 21:20:04 +0800

    文件操作之with方法

--- flush.py
+++ flush.py
@@ -45,16 +45,32 @@
 #     if number==6:
 #        f.write('faning')
 
-f_read=open('寄芸','r',encoding='utf-8')
-f_write=open('寄芸1','w',encoding='utf-8')
-number=0
-for line in f_read:
-    number+=1
-    if number==5:
-        line='hello faning'
-    f_write.write(line)
-f_read.close()
-f_write.close()
+# f_read=open('寄芸','r',encoding='utf-8')
+# f_write=open('寄芸1','w',encoding='utf-8')
+# number=0
+# for line in f_read:
+#     number+=1
+#     if number==5:
+#         line='hello faning'
+#     f_write.write(line)
+# f_read.close()
+# f_write.close()
+
+
+# with语句
+# 为了避免打开文件后忘记关闭，可以通过管理上下文，即：
+# with open('log','r') as f:
+#     pass
+# 如此方式，当with代码块执行完毕时，内部会自动关闭并释放文件资源、
+# 在python2.7后，whih又支持同时对多个文件 的上下文 进行管理，即：
+# whih open('log1') as obj1, open('log2') as obj2
+#     pass
+
+
+with open('log1','r') as f_read,open('log2','w') as f_write:
+    for line in f_read:
+        f_write.write(line)
+
 
 
 
