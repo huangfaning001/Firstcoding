ee2d96299e500639125737e98a56ca467f89c6cd
FaningHuang <33030518+HuangFaning@users.noreply.github.com>
2017-11-25 23:11:17 +0800

    Linux常用命令0

---
+++ Linux常用命令.py
@@ -0,0 +1,43 @@
+#__author__:'Faning Huang'
+#__time__:'2017/11/25 0025 下午 11:06'
+
+
+# 作者：Vamei 出处：http://www.cnblogs.com/vamei 欢迎转载，也请保留这段声明。谢谢！
+# $ 命令行提示符
+#
+# 粗体表示命令
+#
+# 斜体表示参数
+#
+#     filename, file1, file2 都是文件名。有时文件名有后缀，比如file.zip
+#
+#     command 命令名
+#
+#     dir 文件夹名
+#
+#     string 字符串
+#
+#     username 用户名
+#
+#     groupname 组名
+#
+#     regex 正则表达式
+#
+#     path 路径
+#
+#
+#     device 设备名
+#
+#     partition 分区名
+#
+#     IP IP地址
+#
+#     domain 域名
+#
+#     ID 远程用户ID
+#
+#     host 主机名，可以为IP地址或者域名
+#
+#     var 变量名
+#
+#     value 变量值
